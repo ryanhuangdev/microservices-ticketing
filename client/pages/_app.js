91c24eb0c427456c3a4a3c5c8d8541d20711bbc9
Ryan Huang <ryan_huang@intuit.com>
2020-11-28 18:39:42 -0800

    added ui for header and signout

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
-const App = ({ Component, pageProps }) => {
-    return <Component {...pageProps} />;
-}
+import buildClient from "../api/build-client";
+import Header from "../components/header";
+
+const App = ({ Component, pageProps, currentUser }) => {
+  return (
+    <div>
+      <Header currentUser={currentUser} />
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+App.getInitialProps = async (context) => {
+  const client = buildClient(context.ctx);
+  const { data } = await client.get("/api/users/currentuser");
+
+  let pageProps = {};
+  if (context.Component.getInitialProps) {
+    pageProps = await context.Component.getInitialProps(context.ctx);
+  }
+
+  return {
+    pageProps,
+    ...data,
+  };
+};
 
 export default App;
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,18 @@
-import React from 'react';
+import React from "react";
+import buildClient from "../api/build-client";
 
-export default () => {
-    return <h1>Landing</h1>;
-}
+const LandingPage = ({ currentUser }) => {
+  return currentUser ? (
+    <h1>You are signed in</h1>
+  ) : (
+    <h1>You are not signed in</h1>
+  );
+};
+
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
+};
+
+export default LandingPage;
